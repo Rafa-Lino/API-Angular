61b6d05bdd565253ef405d84e2c4dbf3c3f743e9
Rafa-Lino <rafaelino71@gmail.com>
2022-12-20 10:33:50 -0300

    Estabelecendo conexão com o banco de dados

---
+++ php/conexao.php
@@ -0,0 +1,17 @@
+<?php 
+
+//var de acesso
+
+$url = "localhost";
+$usuario = "root";
+$senha = "";
+$base = "api";
+
+//conexão
+
+$conexao = mysqli_connect($url, $usuario, $senha, $base);
+
+
+
+
+?>
